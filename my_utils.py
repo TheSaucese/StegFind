12cb93b8ec499a3ac18d4889c613c8b4b7e632bc
TheSaucese <88286462+TheSaucese@users.noreply.github.com>
2024-03-05 15:16:09 +0100

    Add files via upload

--- my_utils.py
+++ my_utils.py
@@ -1,11 +1,12 @@
 import hashlib
 import os
+from backend.modules.binwalker import extract_with_binwalk
 from backend.modules.channel import process_image
 from backend.modules.embed import extract_with_optional_passphrase
 from backend.modules.hexdump import hexdump
 from backend.modules.lsb import findLSB
 from backend.modules.spectro import create_spectrogram
-from backend.modules.strings import strings
+from backend.modules.strings import extract_strings
 from backend.modules.exif import exif
 from flask import jsonify
 
@@ -34,7 +35,7 @@ def handleFileType(file_path,ext):
     else:
         print(Type)
         print("Unknown")
-        return None
+        return handleUnknown(file_path)
 
 def hashAndUpload(file,ext):
 
@@ -43,41 +44,58 @@ def hashAndUpload(file,ext):
     folder = f"static/uploads/{hash_file}"
     if(ext == 'x-wav'):
         ext = 'wav'
+    if(ext == 'jpeg'):
+        ext = 'jpg'
     file_path = os.path.join(folder, f"file.{ext}")
-    if not os.path.isdir(folder): 
-        os.mkdir(folder)
+    if os.path.isfile(file_path):
+        return file_path,True
+    else:
+        if not os.path.isdir(folder): 
+            os.mkdir(folder)
         file.save(file_path)
 
-    return file_path
+    return file_path,False
 
 def handleImage(file_path):
 
     view_data = process_image(file_path)
     exif_data = exif(file_path)  
     hex_data = hexdump(file_path, bytes_per_line=16)
-    string_data = strings(file_path)
+    string_data = extract_strings(file_path)
     lsb_data = findLSB(file_path)
     embed_data = extract_with_optional_passphrase(file_path)
+    binwalk_data = extract_with_binwalk(file_path)
 
-    return jsonify(viewOutput=view_data,exifOutput=exif_data,contentOutput=hex_data,stringOutput=string_data,lsbOutput=lsb_data,embedOutput=embed_data,file_path=file_path)
+    return jsonify(viewOutput=view_data,exifOutput=exif_data,contentOutput=hex_data,stringOutput=string_data,lsbOutput=lsb_data,embedOutput=embed_data,file_path=file_path,binwalkOutput=binwalk_data)
 
 def handleAudio(file_path):
 
     exif_data = exif(file_path)  
     hex_data = hexdump(file_path, bytes_per_line=16)
-    string_data = strings(file_path)
-
+    string_data = extract_strings(file_path)
+    embed_data = extract_with_optional_passphrase(file_path)
     output_path = getFolder(file_path)
     spectrogram = create_spectrogram(file_path,output_path+"spectrogram.png")
+    binwalk_data = extract_with_binwalk(file_path)
 
-    return jsonify(exifOutput=exif_data,contentOutput=hex_data,stringOutput=string_data,img_data=spectrogram)
+    return jsonify(exifOutput=exif_data,contentOutput=hex_data,stringOutput=string_data,img_data=spectrogram,embedOutput=embed_data,file_path=file_path,binwalkOutput=binwalk_data)
 
 def handleVideo(file_path):
 
-    exif_data = exif(file_path)  
+    exif_data = exif(file_path) 
     hex_data = hexdump(file_path, bytes_per_line=16)
-    string_data = strings(file_path)
+    string_data = extract_strings(file_path)
+    embed_data = extract_with_optional_passphrase(file_path)
+    binwalk_data = extract_with_binwalk(file_path)
+    return jsonify(exifOutput=exif_data,contentOutput=hex_data,stringOutput=string_data,embedOutput=embed_data,file_path=file_path,binwalkOutput=binwalk_data)
 
-    return jsonify(exifOutput=exif_data,contentOutput=hex_data,stringOutput=string_data)
 
+def handleUnknown(file_path):
 
+    exif_data = exif(file_path) 
+    hex_data = hexdump(file_path, bytes_per_line=16)
+    string_data = extract_strings(file_path)
+    embed_data = extract_with_optional_passphrase(file_path)
+    binwalk_data = extract_with_binwalk(file_path)
+
+    return jsonify(exifOutput=exif_data,contentOutput=hex_data,stringOutput=string_data,embedOutput=embed_data,file_path=file_path,binwalkOutput=binwalk_data)
